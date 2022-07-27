608cb3a683dc10baa6350f6f8ae767193dc82df4
niscolas <nic.cp@hotmail.com>
2022-07-26 21:35:43 -0300

    misc: updates

--- .config/rofi/scripts/xrandrlayoutmenu.sh
+++ .config/rofi/scripts/xrandrlayoutmenu.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-if [ $@ == "default" ]; then
+if [ $@ == "dual-120" ]; then
     xrandr --output HDMI-1-0 --primary --mode 1920x1080 --rate 120 --left-of eDP-1 \
             --output eDP-1 --mode 1920x1080 --rate 120
     exit 0
-elif [[ $@ == "hdmi-only" ]]; then
+elif [ $@ == "dual-60" ]; then
+    xrandr --output HDMI-1-0 --primary --mode 1920x1080 --rate 60 --left-of eDP-1 \
+            --output eDP-1 --mode 1920x1080 --rate 60
+    exit 0
+elif [[ $@ == "hdmi-120" ]]; then
     xrandr --output HDMI-1-0 --primary --mode 1920x1080 --rate 120 \
             --output eDP-1 --off
     exit 0
-elif [[ $@ == "internal-only" ]]; then
+elif [[ $@ == "hdmi-60" ]]; then
+    xrandr --output HDMI-1-0 --primary --mode 1920x1080 --rate 60 \
+            --output eDP-1 --off
+    exit 0
+elif [[ $@ == "internal-120" ]]; then
     xrandr --output eDP-1 --primary --mode 1920x1080 --rate 120 \
+            --output HDMI-1-0 --off
+            
+    exit 0
+elif [[ $@ == "internal-60" ]]; then
+    xrandr --output eDP-1 --primary --mode 1920x1080 --rate 60 \
             --output HDMI-1-0 --off
             
     exit 0
 fi
 
-echo "default"
-echo "hdmi-only"
-echo "internal-only"
+echo "dual-120"
+echo "dual-60"
+echo "hdmi-120"
+echo "hdmi-60"
+echo "internal-120"
+echo "internal-60"
--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -25,24 +25,5 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 
 bindkey -v
 
-### ctrl+arrows
-bindkey "\e[1;5C" forward-word
-bindkey "\e[1;5D" backward-word
-# urxvt
-bindkey "\eOc" forward-word
-bindkey "\eOd" backward-word
-
-### ctrl+delete
-bindkey "\e[3;5~" kill-word
-# urxvt
-bindkey "\e[3^" kill-word
-
-### ctrl+backspace
-bindkey '^H' backward-kill-word
-
-### ctrl+shift+delete
-bindkey "\e[3;6~" kill-line
-# urxvt
-bindkey "\e[3@" kill-line
-
+prompt off
 eval "$(starship init zsh)"
