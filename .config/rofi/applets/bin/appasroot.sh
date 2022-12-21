3c0ab473cbdb6e02beaaafb5b5fb3aae14771cce
niscolas <nic.cp@hotmail.com>
2022-12-21 10:24:56 -0300

    feat: updates rofi config

--- .config/rofi/applets/bin/appasroot.sh
+++ .config/rofi/applets/bin/appasroot.sh
@@ -14,85 +14,99 @@ prompt='Applications'
 mesg='Run Applications as Root'
 
 if [[ "$theme" == *'type-1'* ]]; then
-	list_col='1'
-	list_row='5'
-	win_width='400px'
+    list_col='1'
+    list_row='6'
+    win_width='400px'
 elif [[ "$theme" == *'type-3'* ]]; then
-	list_col='1'
-	list_row='5'
-	win_width='120px'
+    list_col='1'
+    list_row='6'
+    win_width='120px'
 elif [[ "$theme" == *'type-5'* ]]; then
-	list_col='1'
-	list_row='5'
-	win_width='520px'
+    list_col='1'
+    list_row='6'
+    win_width='520px'
 elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
-	list_col='5'
-	list_row='1'
-	win_width='670px'
+    list_col='6'
+    list_row='1'
+    win_width='670px'
 fi
 
 # Options
 layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
 if [[ "$layout" == 'NO' ]]; then
-	option_1="stop keyd"
-	option_2=" start keyd"
-	option_3="stop picom"
-	option_4=" start picom"
+    option_1=" keyd stop"
+    option_2=" keyd start"
+    option_3=" picom stop"
+    option_4=" picom start"
+    option_5=" barrier left right"
+    option_6=" barrier up down"
 else
-	option_1=""
-	option_2=""
-	option_3=""
-	option_4=""
+    option_1=""
+    option_2=""
+    option_3=""
+    option_4=""
+    option_5=""
+    option_6=""
 fi
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -theme-str "window {width: $win_width;}" \
-		-theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		-markup-rows \
-		-theme ${theme}
-}
+    rofi -theme-str "window {width: $win_width;}" \
+        -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+        -theme-str 'textbox-prompt-colon {str: "";}' \
+        -dmenu \
+        -p "$prompt" \
+        -mesg "$mesg" \
+        -markup-rows \
+        -theme ${theme}
+    }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
+    echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	polkit_cmd="pkexec env PATH=$PATH DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY"
-	if [[ "$1" == '--opt1' ]]; then
-		${polkit_cmd} systemctl stop keyd
-	elif [[ "$1" == '--opt2' ]]; then
-		${polkit_cmd} systemctl start keyd
-	elif [[ "$1" == '--opt3' ]]; then
-		${polkit_cmd} killall picom
-	elif [[ "$1" == '--opt4' ]]; then
-		${polkit_cmd} picom &
-	fi
+    polkit_cmd="pkexec env PATH=$PATH DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY"
+    if [[ "$1" == '--opt1' ]]; then
+        ${polkit_cmd} systemctl stop keyd
+    elif [[ "$1" == '--opt2' ]]; then
+        ${polkit_cmd} systemctl start keyd
+    elif [[ "$1" == '--opt3' ]]; then
+        killall picom
+    elif [[ "$1" == '--opt4' ]]; then
+        picom
+    elif [[ "$1" == '--opt5' ]]; then
+        notify-send "5"
+        kitty -e $HOME/scripts/barrier/work_server-left_right.sh
+    elif [[ "$1" == '--opt6' ]]; then
+        notify-send "6"
+        kitty -e $HOME/scripts/barrier/work_server-up_down.sh
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
     $option_1)
-		run_cmd --opt1
+        notify-send "1"
+        run_cmd --opt1
         ;;
     $option_2)
-		run_cmd --opt2
+        run_cmd --opt2
         ;;
     $option_3)
-		run_cmd --opt3
+        run_cmd --opt3
         ;;
     $option_4)
-		run_cmd --opt4
+        run_cmd --opt4
         ;;
     $option_5)
-		run_cmd --opt5
+        run_cmd --opt5
+        ;;
+    $option_6)
+        run_cmd --opt6
         ;;
 esac
 
