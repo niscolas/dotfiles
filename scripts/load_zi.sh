b3de428bce785a2f65a5120b42b9b57be258c92a
niscolas <nic.cp@hotmail.com>
2022-12-21 10:50:55 -0300

    fix: load_zi.sh for real

--- scripts/load_zi.sh
+++ scripts/load_zi.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 zi_home_path="$ZI_HOME/bin/zi.zsh"
-if ! [ -f  $zi_home_path ]; then
-    exit
+if [ -f  $zi_home_path ]; then
+    source $zi_home_path  
+
+    zi ice wait
+    zi load "zsh-users/zsh-autosuggestions"
+    zi load "zsh-users/zsh-syntax-highlighting"
 fi
 
-source $zi_home_path  
-
-zi ice wait
-zi load "zsh-users/zsh-autosuggestions"
-zi load "zsh-users/zsh-syntax-highlighting"
