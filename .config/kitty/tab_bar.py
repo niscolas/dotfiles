d33dd885447bf25c8ae16ab85944013667de60fe
niscolas <nic.cp@hotmail.com>
2022-12-26 14:59:12 -0300

    fix: tab bar for linux

--- .config/kitty/tab_bar.py
+++ .config/kitty/tab_bar.py
@@ -2,6 +2,10 @@
 
 from kitty.fast_data_types import Screen
 from kitty.tab_bar import DrawData, ExtraData, TabBarData, draw_title
+from kitty.utils import color_as_int
+
+def as_rgb(x: int) -> int:
+    return (x << 8) | 2
 
 def draw_tab(
     draw_data: DrawData, screen: Screen, tab: TabBarData,
@@ -13,34 +17,25 @@ def draw_tab(
     left_sep, right_sep = ('', '')
 
     def draw_sep(which: str) -> None:
-        screen.cursor.bg = draw_data.default_bg
+        screen.cursor.bg = as_rgb(color_as_int(draw_data.default_bg))
         screen.cursor.fg = orig_bg
         screen.draw(which)
         screen.cursor.bg = orig_bg
         screen.cursor.fg = orig_fg
 
-    if max_title_length <= 1:
+    draw_sep(left_sep)
+    screen.draw(' ')
+    draw_title(draw_data, screen, tab, index)
+    extra = screen.cursor.x - before - max_title_length
+    print("extra:%d" %(extra))
+    if extra >= 0:
+        screen.cursor.x -= extra + 3
         screen.draw('…')
-    elif max_title_length == 2:
-        screen.draw('…|')
-    elif max_title_length < 6:
-        draw_sep(left_sep)
-        screen.draw((' ' if max_title_length == 5 else '') + '…' + (' ' if max_title_length >= 4 else ''))
-        draw_sep(right_sep)
-    else:
-        draw_sep(left_sep)
-        screen.draw(' ')
-        draw_title(draw_data, screen, tab, index)
-        extra = screen.cursor.x - before - max_title_length
-        print("extra:%d" %(extra))
-        if extra >= 0:
-            screen.cursor.x -= extra + 3
-            screen.draw('…')
-        elif extra == -1:
-            screen.cursor.x -= 2
-            screen.draw('…')
-        screen.draw(' ')
-        draw_sep(right_sep)
-        draw_sep(' ')
+    elif extra == -1:
+        screen.cursor.x -= 2
+        screen.draw('…')
+    screen.draw(' ')
+    draw_sep(right_sep)
+    draw_sep(' ')
 
     return screen.cursor.x
