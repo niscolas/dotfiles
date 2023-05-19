55bf8ebd8672024135b83917e7770b307253cf22
niscolas <nic.cp@hotmail.com>
2023-05-19 11:43:30 -0300

    feat: adds wezterm config

---
+++ .config/wezterm/tab_bar.lua
@@ -0,0 +1,76 @@
+local M = {}
+
+M.setup = function(color_scheme)
+	local wezterm = require("wezterm")
+
+	local left_half_circle = wezterm.nerdfonts.ple_left_half_circle_thick
+	local right_half_circle = wezterm.nerdfonts.ple_right_half_circle_thick
+	local left_half_circle_thin = wezterm.nerdfonts.ple_left_half_circle_thin
+	local right_half_circle_thin = wezterm.nerdfonts.ple_right_half_circle_thin
+	local divider_spacing = 2
+
+	-- This function returns the suggested title for a tab.
+	-- It prefers the title that was set via `tab:set_title()`
+	-- or `wezterm cli set-tab-title`, but falls back to the
+	-- title of the active pane in that tab.
+	function tab_title(tab_info)
+		local title = tab_info.tab_title
+		if title and #title > 0 then
+			return title
+		end
+
+		local current_working_dir = tab_info.active_pane.current_working_dir
+		local home_dir = os.getenv("HOME")
+		title = current_working_dir
+		title = string.gsub(title, "file://", "")
+		title = string.sub(title, string.find(title, "/"), string.len(title))
+
+		if home_dir then
+			title = string.gsub(title, home_dir, "$HOME")
+		end
+
+		return title
+	end
+
+	wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+		local edge_background = color_scheme.background
+		local background = color_scheme.background
+		local foreground = color_scheme.foreground
+
+		local edge_foreground = background
+
+		local title = tab_title(tab)
+
+		-- ensure that the titles fit in the available space,
+		-- and that we have room for the edges.
+		title = wezterm.truncate_right(title, max_width - 8)
+
+		local index_bg = tab.is_active and color_scheme.brights[4] or color_scheme.brights[5]
+
+		return {
+			{ Attribute = { Intensity = "Bold" } },
+			{ Background = { Color = edge_background } },
+			{ Foreground = { Color = index_bg } },
+			{ Text = left_half_circle },
+			{ Attribute = { Intensity = "Bold" } },
+			{ Background = { Color = index_bg } },
+			{ Foreground = { Color = background } },
+			{ Text = tostring(tab.tab_index + 1) },
+			{ Background = { Color = background } },
+			{ Foreground = { Color = index_bg } },
+			{ Text = right_half_circle },
+			{ Attribute = { Intensity = "Bold" } },
+			{ Background = { Color = background } },
+			{ Foreground = { Color = index_bg } },
+			{ Text = " " .. title },
+			{ Background = { Color = index_bg } },
+			{ Foreground = { Color = edge_background } },
+			{ Text = right_half_circle },
+			{ Background = { Color = edge_background } },
+			{ Foreground = { Color = index_bg } },
+			{ Text = right_half_circle .. "  " },
+		}
+	end)
+end
+
+return M
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,95 @@
+local wezterm = require("wezterm")
+local action = wezterm.action
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font("Caskaydia Cove Nerd Font")
+config.font_size = 10
+config.window_background_opacity = 0.85
+
+config.show_new_tab_button_in_tab_bar = false
+config.tab_and_split_indices_are_zero_based = false
+config.tab_max_width = 48
+config.use_fancy_tab_bar = false
+
+local color_scheme_name = "GruvboxDark"
+local color_scheme = wezterm.color.get_builtin_schemes()[color_scheme_name]
+config.color_scheme = color_scheme_name
+config.colors = {
+	tab_bar = {
+		background = color_scheme.background,
+	},
+}
+
+config.keys = {
+	{ key = "{", mods = "CTRL|SHIFT", action = action.MoveTabRelative(-1) },
+	{ key = "}", mods = "CTRL|SHIFT", action = action.MoveTabRelative(1) },
+}
+
+--
+--ansi = [
+-- '#1e1e1e',
+--     '#be0f17',
+--     '#868715',
+--     '#cc881a',
+--     '#377375',
+--     '#a04b73',
+--     '#578e57',
+--     '#978771',
+-- ]
+-- background = '#1e1e1e'
+-- brights = [
+--     '#7f7061',
+--     '#f73028',
+--     '#aab01e',
+--     '#f7b125', '#719586',
+--     '#c77089',
+--     '#7db669',
+--     '#e6d4a3',
+-- ]
+-- cursor_bg = '#e6d4a3'
+-- cursor_border = '#e6d4a3'
+-- cursor_fg = '#1e1e1e'
+-- foreground = '#e6d4a3'
+-- selection_bg = '#e6d4a3'
+-- selection_fg = '#534a42'
+--
+
+require("tab_bar").setup(color_scheme)
+
+local accent_color = color_scheme.brights[6]
+local secondary_color = color_scheme.background
+
+-- Equivalent to POSIX basename(3)
+-- Given "/foo/bar" returns "bar"
+-- Given "c:\\foo\\bar" returns "bar"
+function basename(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+wezterm.on("update-right-status", function(window, pane)
+	window:set_right_status(wezterm.format({
+		{ Background = { Color = secondary_color } },
+		{ Foreground = { Color = accent_color } },
+		{ Text = wezterm.nerdfonts.ple_left_half_circle_thick },
+
+		{ Background = { Color = accent_color } },
+		{ Foreground = { Color = secondary_color } },
+		{ Attribute = { Intensity = "Bold" } },
+		{ Text = wezterm.nerdfonts.dev_terminal },
+
+		{ Background = { Color = secondary_color } },
+		{ Foreground = { Color = accent_color } },
+		{ Text = wezterm.nerdfonts.ple_right_half_circle_thick },
+
+		{ Background = { Color = secondary_color } },
+		{ Foreground = { Color = accent_color } },
+		{ Text = " " .. basename(pane:get_foreground_process_name()) .. " " },
+	}))
+end)
+
+return config
