275e233075cff8f4bae45d86b58aa9220eee5063
niscolas <nic.cp@hotmail.com>
2023-06-13 19:45:39 -0300

    feat(wezterm): font update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,14 +7,15 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font("Caskaydia Cove Nerd Font")
-config.font_size = 10
+config.font = wezterm.font("IntelOne Mono", { weight = "Medium" })
+config.font_size = 11
 config.window_background_opacity = 0.85
 
 config.show_new_tab_button_in_tab_bar = false
 config.tab_and_split_indices_are_zero_based = false
 config.tab_max_width = 48
 config.use_fancy_tab_bar = false
+config.adjust_window_size_when_changing_font_size = false
 
 local color_scheme_name = "GruvboxDark"
 local color_scheme = wezterm.color.get_builtin_schemes()[color_scheme_name]
