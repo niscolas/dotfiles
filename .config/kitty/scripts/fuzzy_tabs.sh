c3edb8b902e8d3767761434ab85f571a1e8515d8
niscolas <nic.cp@hotmail.com>
2023-06-16 20:11:53 -0300

    feat(kitty): removes config

--- .config/kitty/scripts/fuzzy_tabs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#obtained from: https://github.com/kovidgoyal/kitty/issues/1303#issuecomment-1311202869
-
-fzf --with-nth 2.. --bind 'enter:execute-silent(kitty @ focus-tab -m id:{1})+accept' > /dev/null \
-    <<<$(kitty @ ls | jq -r '.[] | .tabs[] | select(.is_focused == false) | (.id|tostring) + " " + .title')
